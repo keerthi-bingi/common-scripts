1136bb140d484bd4795aa0e9fb40c5b4c49fe02c
keerthi-bingi <keerthibingi1312@gmail.com>
2026-02-03 20:39:52 +0530

    Adding scripts

--- common.sh
+++ common.sh
@@ -8,6 +8,7 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 SCRIPT_DIR="$PWD"
+MONGODB_HOST="mongodb.bingi.online"
 START_TIME="$(date +%s)"
 
 mkdir -p $LOGS_FOLDER
@@ -28,6 +29,60 @@ VALIDATE(){
     else
         echo -e "$2 ... $G SUCCESS $N" | tee -a $LOGS_FILE
     fi
+}
+
+NODE_JS_SETUP(){
+    dnf module disable nodejs -y &>>$LOGS_FILE
+    VALIDATE $? "Disabling nodejs latest version"
+
+    dnf module enable nodejs:20 -y &>>$LOGS_FILE
+    VALIDATE $? "Enabling nodejs 20"
+
+    dnf install nodejs -y &>>$LOGS_FILE
+    VALIDATE $? "Installing nodejs 20"
+
+    npm install  &>>$LOGS_FILE
+    VALIDATE $? "Installing dependencies"
+}
+
+APP_SETUP(){
+    id roboshop &>>$LOGS_FILE
+    if [ $? -ne 0 ]; then
+        useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOGS_FILE
+        VALIDATE $? "Creating system user"
+    else
+        echo -e "Roboshop user already exist ... $Y SKIPPING $N"
+    fi
+
+    mkdir -p /app 
+    VALIDATE $? "Creating app directory"
+
+    curl -o /tmp/$APP_NAME.zip https://roboshop-artifacts.s3.amazonaws.com/$APP_NAME-v3.zip  &>>$LOGS_FILE
+    VALIDATE $? "Downloading $APP_NAME code"
+
+    cd /app
+    VALIDATE $? "Moving to app directory"
+
+    rm -rf /app/*
+    VALIDATE $? "Removing existing code"
+
+    unzip /tmp/$APP_NAME.zip &>>$LOGS_FILE
+    VALIDATE $? "Uzip $APP_NAME code"
+}
+
+SYSTEMD_SETUP(){
+    cp $SCRIPT_DIR/$APP_NAME.service /etc/systemd/system/$APP_NAME.service
+    VALIDATE $? "Created systemctl service"
+
+    systemctl daemon-reload
+    systemctl enable $APP_NAME  &>>$LOGS_FILE
+    systemctl start $APP_NAME
+    VALIDATE $? "Starting and enabling $APP_NAME"
+}
+
+APP_RESTART(){
+    systemctl restart $APP_NAME
+    VALIDATE $? "Restarting $APP_NAME"
 }
 
 PRINT_TOTAL_TIME(){
